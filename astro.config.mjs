19571cfd40d55f4ff8efb3fdf046dc3d5283c698
jasonmitchellwebdeveloper <124547967+jasonmitchellwebdeveloper@users.noreply.github.com>
2025-04-03 06:52:21 +1000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import alpine from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-cc.thewebdeveloper.com.au/',
+  site: 'https://astro-cc.thewebdeveloper.com.au',
   integrations: [react(), tailwind(), bookshop(), alpine(), mdx()],
-  base: 'astro-cc.thewebdeveloper.com.au',
-  trailingSlash: "always"
 });
