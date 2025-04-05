6a0432d57528202ec66af39e452dd32d97d2fc56
Jason Mitchell <jasonmitchellwebdeveloper@gmail.com>
2025-04-05 22:14:01 +1000

    remove deploy scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import bookshop from '@bookshop/astro-bookshop';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import alpine from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import bookshop from "@bookshop/astro-bookshop";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import alpine from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-cc.thewebdeveloper.com.au',
+  site: "astro-cc.thewebdeveloper.com.au",
   integrations: [react(), tailwind(), bookshop(), alpine(), mdx()],
-  outDir: './web',
 });
